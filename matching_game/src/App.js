4a398ad9d23c6bb3731a970e145e82c447fe2c82
Danailnd <danaildimitrov01@gmail.com>
2021-09-01 14:03:25 +0200

    Update App.js

--- matching_game/src/App.js
+++ matching_game/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import test from './custom_modules/test.js';
+import Paper from '@material-ui/core/Paper';
 
 
 
-  const test1 = {
-    backgroundColor: 'red',
-    fontSize: 33
+  const classes = {
+    test : {
+    fontSize: 33},
   }
 
  
@@ -15,9 +16,12 @@ import test from './custom_modules/test.js';
 function App() {
   const dog = test();
   return (
-    <div>
-    <h3 style = {test1}>Matching Game</h3>
+    <>
+    <Paper elevation={0} />
+    <div className="App" >
+    <h3 style = {classes.test}>Matching Game</h3>
     </div>
+    </>
   );
 }
 
