adfbfde0aed2f6691f0408a0d5780a0359222d2b
Danailnd <danaildimitrov01@gmail.com>
2021-09-01 20:04:36 +0200

    ehh.....

--- matching_game/src/App.js
+++ matching_game/src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
-import test from './custom_modules/test.js';
+import game_setup from './custom_modules/game_setup.jsx';
 import Paper from '@material-ui/core/Paper';
 
-
-
-  const classes = {
-    test : {
-    fontSize: 33},
-  }
-
- 
-  
-
-
 function App() {
-  const dog = test();
   return (
     <>
-    <div className="Header" >
-    <h3 style = {classes.test}>Matching Game</h3>
-    </div>
-    <div >
-      <h3>Test1</h3>
-      <h3>Test2</h3>
+    <h3 className="Header">Matching Game</h3>
+    <div className="container">
+
     </div>
     </>
   );
--- matching_game/src/custom_modules/test.js
+++
@@ -1,4 +0,0 @@
-export default function test(){
-    let dog = "dog";
-    return dog;
-}
